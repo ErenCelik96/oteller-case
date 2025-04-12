31313ac9e75b47863c96555e5b6fcdef9dbabbd0
ErenCelik96 <celik.erenn96@gmail.com>
2025-04-12 23:13:21 +0300

    added types

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export enum Language {
+  EN = "en",
+  TR = "tr",
+}
+
+export enum Currency {
+  USD = "USD",
+  EUR = "EUR",
+  TRY = "TRY",
+}
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
