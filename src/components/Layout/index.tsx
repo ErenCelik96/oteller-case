44209f978282690cfe362a2b1716dd79de787e25
ErenCelik96 <celik.erenn96@gmail.com>
2025-04-13 21:15:28 +0300

    created selector component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./Layout.module.css";
+import { Selector } from "../Selector";
 
 export const Layout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  return <div className={styles["layout"]}>Hello {children}</div>;
+  return (
+    <div className={styles["layout"]}>
+      Hello
+      <Selector type="lang" />
+      <Selector type="currency" />
+      {children}
+    </div>
+  );
 };
---
+++ src/components/Selector/index.tsx
@@ -0,0 +1,47 @@
+import React, { useMemo } from "react";
+import { useAppStore } from "@/store/store";
+import { Currency, Language, Symbols } from "@/types/types";
+import { useRouter } from "next/router";
+
+export const Selector: React.FC<{
+  type: "lang" | "currency";
+}> = ({ type }) => {
+  const router = useRouter();
+  const { language, setLanguage, currency, setCurrency } = useAppStore();
+
+  const currentValue = type === "lang" ? language : currency;
+
+  const options = useMemo(() => {
+    if (type === "lang") {
+      return [
+        { value: Language.EN, symbol: "English" },
+        { value: Language.TR, symbol: "Türkçe" },
+      ];
+    } else {
+      return [
+        { value: Currency.USD, symbol: Symbols.USD },
+        { value: Currency.EUR, symbol: Symbols.EUR },
+        { value: Currency.TRY, symbol: Symbols.TRY },
+      ];
+    }
+  }, [type]);
+
+  const handleChange = (value: string) => {
+    if (type === "lang") {
+      setLanguage(value as Language);
+    } else {
+      setCurrency(value as Currency);
+      router.replace(router.asPath);
+    }
+  };
+
+  return (
+    <select value={currentValue} onChange={(e) => handleChange(e.target.value)}>
+      {options?.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.symbol}
+        </option>
+      ))}
+    </select>
+  );
+};
